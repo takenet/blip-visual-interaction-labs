9712ccde7200b65293b3d3c179ec042ded808666
Willian Lomeu <willianlomeu.contato@gmail.com>
2022-10-05 10:05:10 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 class App extends React.Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <MainRoutes />
       </Router>
         
