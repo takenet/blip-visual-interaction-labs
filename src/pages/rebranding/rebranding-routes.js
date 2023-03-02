3176c9f1551518e50c13cecb4bb25572390686a7
Lucas Murta <lucas.murta@take.net>
2023-03-02 17:46:25 -0300

    feat(pages): add components page to test theme

--- src/pages/rebranding/rebranding-routes.js
+++ src/pages/rebranding/rebranding-routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import {Home} from "./pages/home";
+import { Home } from "./pages/home";
+import { Components } from "./pages/components";
 
 export default function RebradingRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/components" element={<Components />} />
     </Routes>
   );
 }
